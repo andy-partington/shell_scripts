7ae8fd6e51c6897c195fa9c76d03dbd93b6abdf9
Andy Partington <andy@agathos.co.uk>
2017-01-30 15:47:34 +0000

    Added some more checks if the curl command worked or the exit status

--- do_dynamic.sh
+++ do_dynamic.sh
@@ -2,7 +2,6 @@
 
 # Update an A record on Ddigital Ocean via their API.
 # API documents here:  https://developers.digitalocean.com/documentation/v2
-# Will update and tidy the code later
 
 # Load config file with api/domain etc
 
@@ -21,22 +20,23 @@ record_id=$RECORD_ID
 # You can create a key in your DO area, follow the doc links above for more info
 api_key=$API_KEY
 
-# Get your current A record for the record IP to compare against your current IP
-# This is dependant on a program called jq ( apt install jq )
-#currentip="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id" | jq -r ".domain_record"  | jq -r ".data")"
-currentip="82.69.12.215"
-echo $currentip
+# Get your current A record for the record_id to compare against your current IP
+# This is dependant on a program called jq ( apt install jq ) may do this differently in the future.
+currentip="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id" | jq -r ".domain_record"  | jq -r ".data")"
 
 # Retrieve your current IP
 # The -s makes it quiet and the -4 for IPv4
 ip="$(curl -s -4 icanhazip.com)"
 
-echo $ip
-
 if [ "$currentip" != "$ip" ] ; then 
-    echo "IP has changed"
+    echo "IP Address differs from the A record, updating A record"
+    curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" -d "{\"data\":\"$ip\"}" "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id"
+    exit_status = $?
+	if [ $exit_status != 0 ]
+	  then
+	    exit $exit_status
+	else
+    	echo "IP Address updated successfully"
 else
-    echo "Still the same..."
+    echo "IP Address not changed"
 fi
-# Now push the data to DO
-#curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" -d "{\"data\":\"$ip\"}" "https://api.digitalocean.com/v2/domains/$domain_id/records/$record_id"
